240f72c76787ce885bd4fe39abce059572ae582b
arbdigital-art <arb.digitalart@gmail.com>
2024-12-04 20:26:20 -0300

    Packer-manager

---
+++ lua/core/plugin_config/treesitter.lua
@@ -0,0 +1,10 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+  },
+}
