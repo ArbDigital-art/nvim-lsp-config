f3ac61c82e0d3858e8473bc0fb076bde381420d3
arbdigital-art <arb.digitalart@gmail.com>
2024-12-05 15:29:08 -0300

    update keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,6 +2,13 @@
 --
 local map = vim.keymap.set
 
+-- source
+map('', '<leader>rr', ':source %<cr>', { desc = "Source Update"} )
 
 -- ToggleTerm
 map('n', '<leader>\\', ':ToggleTerm size=10 direction=horizontal name=Terminal<cr>', { desc = 'Horizontal Terminal' })
+
+--Tab
+map('n', '<leader>t',  ':tabnew<cr>',   { desc = "New Tab" } )
+map('n', '<leader>tn', ':tabnext<cr>',  { desc = "Next tab" } )
+map('n', '<leader>tc', ':tabclose<cr>', { desc = "Close Tab" } )
